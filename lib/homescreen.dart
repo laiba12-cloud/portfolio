120b21edda19d8b02c1179ca032c55c562836fc9
laiba12-cloud <laibayounis123@gmail.com>
2025-07-26 15:34:30 +0500

    updated code

---
+++ lib/homescreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class PorrtfolioClass extends StatelessWidget {
+  const PorrtfolioClass({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Expanded(
+          flex:50,
+          child: Column(
+        children: [
+
+        ],
+      ))
+    );
+  }
+}
